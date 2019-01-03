f9a8d8e0694a2dc4509f406e66c48d393c5df5ad
Nick Giacobe <nxg13@ist.psu.edu>
2019-01-03 18:12:46 -0500

    Update level0.sh

--- level0.sh
+++ level0.sh
@@ -3,6 +3,7 @@
 cp Instructions.txt /home/level0
 
 echo "The password for the next level is in a file called inhere.txt" > /home/level0/README.txt;
+echo "cat ~/README.txt" > .bashrc
 
 echo $level1_pass > /home/level0/inhere.txt;
 chown level0:level0 /home/level0/inhere.txt;
