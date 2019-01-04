edc0d6f47f1087f077fd8be292cedf27e08ae7ce
Nick Giacobe <nxg13@ist.psu.edu>
2019-01-04 16:48:10 -0500

    Update level7.sh

--- level7.sh
+++ level7.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-echo "Your password is in the inhere.txt file. However, it is not so easy to read the file." > /home/level7/README.txt;
-
+echo "******************************************************************" >> /home/level7/README.txt;
+echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level7/README.txt;
+echo "* Overthewire.org's Bandit exercise. The object is to figure out *" >> /home/level7/README.txt;
+echo "* what the password is for the next level, then log into that    *" >> /home/level7/README.txt;
+echo "* next level's account using SSH.                                *" >> /home/level7/README.txt;
+echo "*                                                                *" >> /home/level7/README.txt;
+echo "* You are at Level 7                                             *" >> /home/level7/README.txt;
+echo "*                                                                *" >> /home/level7/README.txt;
+echo "* Your password is in the inhere.txt file. However, it is not so *" >> /home/level7/README.txt;
+echo "* easy to read the file.                                         *" >> /home/level7/README.txt;
+echo "*                                                                *" >> /home/level7/README.txt;
+echo "* When you get the password for the next level, log in to the    *" >> /home/level7/README.txt;
+echo "* next level with the command:                                   *" >> /home/level7/README.txt;
+echo "*         ssh level8@localhost                                   *" >> /home/level7/README.txt;
+echo "*                                                                *" >> /home/level7/README.txt;
+echo "******************************************************************" >> /home/level7/README.txt;
+echo "cat /home/level7/README.txt" >> /home/level7/.bashrc
+f
 echo $level8_pass > /home/level7/inhere.txt;
 chown level7:level7 /home/level7/inhere.txt;
 
