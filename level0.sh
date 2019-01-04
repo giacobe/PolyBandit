0ceb47fc769ad79562525e0a48a5526860039bc7
Nick Giacobe <nxg13@ist.psu.edu>
2019-01-04 15:49:41 -0500

    Update level0.sh

--- level0.sh
+++ level0.sh
@@ -3,7 +3,15 @@
 cp Instructions.txt /home/level0
 
 echo "******************************************************************" >> /home/level0/README.txt;
+echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level0/README.txt;
+echo "* Overthewire.org's Bandit exercise. The object is to figure out *" >> /home/level0/README.txt;
+echo "* what the password is for the next level, then log into that    *" >> /home/level0/README.txt;
+echo "* next level's account using SSH.                                *" >> /home/level0/README.txt;
+echo "*                                                                *" >> /home/level0/README.txt;
+echo "* You are at Level 0                                             *" >> /home/level0/README.txt;
+echo "*                                                                *" >> /home/level0/README.txt;
 echo "* The password for the next level is in a file called inhere.txt *" >> /home/level0/README.txt;
+echo "*                                                                *" >> /home/level0/README.txt;
 echo "******************************************************************" >> /home/level0/README.txt;
 echo "cat ~/README.txt" >> /home/level0/.bashrc
 
