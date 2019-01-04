6f11c22fc90dba050f6f4957f26595ce06570db7
Nick Giacobe <nxg13@ist.psu.edu>
2019-01-04 16:07:49 -0500

    Update level2.sh

--- level2.sh
+++ level2.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
+    
+echo "******************************************************************" >> /home/level2/README.txt;
+echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level2/README.txt;
+echo "* Overthewire.org's Bandit exercise. The object is to figure out *" >> /home/level2/README.txt;
+echo "* what the password is for the next level, then log into that    *" >> /home/level2/README.txt;
+echo "* next level's account using SSH.                                *" >> /home/level2/README.txt;
+echo "*                                                                *" >> /home/level2/README.txt;
+echo "* You are at Level 2                                             *" >> /home/level2/README.txt;
+echo "*                                                                *" >> /home/level2/README.txt;
+echo "* The password for the next level is in a file with byte size 9  *" >> /home/level2/README.txt;
+echo "*                                                                *" >> /home/level2/README.txt;
+echo "* When you get the password for the next level, log in to the    *" >> /home/level2/README.txt;
+echo "* next level with the command:                                   *" >> /home/level2/README.txt;
+echo "*         ssh level3@localhost                                   *" >> /home/level2/README.txt;
+echo "*                                                                *" >> /home/level2/README.txt;
+echo "******************************************************************" >> /home/level2/README.txt;
 
-echo "The password for the next level is in a file with byte size 9" > /home/level2/README.txt;
+echo "cat /home/level2/README.txt" >> /home/level2/.bashrc
 
 declare -a LETTERS=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0");
 
@@ -35,6 +51,7 @@ do
       file_string="$file_string${LETTERS[$[RANDOM%61]]}";
       count2=$(($count2+1));
     done
+
     echo $file_string > /home/level2/$rand_file.txt
     chown level2:level2 /home/level2/$rand_file.txt
     count=$(($count+1));
