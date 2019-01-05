11902cd61da60c720a026476a95e1c621029cb48
Nick Giacobe <nxg13@ist.psu.edu>
2019-01-05 18:57:15 -0500

    Update level3.sh

--- level3.sh
+++ level3.sh
@@ -4,7 +4,7 @@ password_spot=$[RANDOM%256];
 
 declare -a LETTERS=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0");
 
-echo > home/level3/oneline.txt;
+touch home/level3/oneline.txt;
 
 count=0;
 while [ $count -lt 256 ];
