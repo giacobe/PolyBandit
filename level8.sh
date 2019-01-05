504c9aab1bada754a0742c636bee6ec96379d6e9
Nick Giacobe <nxg13@ist.psu.edu>
2019-01-04 21:32:35 -0500

    Update level8.sh

--- level8.sh
+++ level8.sh
@@ -2,7 +2,23 @@
 
 useradd level8_ans --create-home --password "$(openssl passwd -1 "password")" --shell /bin/bash --user-group
 
-echo "The password for the next level is in a file called inhere.txt in level8_ans, but there is a problem when you try to log in. The password is password." > /home/level8/README.txt;
+echo "******************************************************************" >> /home/level8/README.txt;
+echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level8/README.txt;
+echo "* Overthewire.org's Bandit exercise. The object is to figure out *" >> /home/level8/README.txt;
+echo "* what the password is for the next level, then log into that    *" >> /home/level8/README.txt;
+echo "* next level's account using SSH.                                *" >> /home/level8/README.txt;
+echo "*                                                                *" >> /home/level8/README.txt;
+echo "* You are at Level 8                                             *" >> /home/level8/README.txt;
+echo "*                                                                *" >> /home/level8/README.txt;
+echo "* The password for the next level is in a file called inhere.txt *" >> /home/level8/README.txt;
+echo "* in level8_ans, but there is a problem when you try to log in.  *" >> /home/level8/README.txt;
+echo "*                                                                *" >> /home/level8/README.txt;
+echo "* When you get the password for the next level, log in to the    *" >> /home/level8/README.txt;
+echo "* next level with the command:                                   *" >> /home/level8/README.txt;
+echo "*         ssh level8@localhost                                   *" >> /home/level8/README.txt;
+echo "*                                                                *" >> /home/level8/README.txt;
+echo "******************************************************************" >> /home/level8/README.txt;
+echo "cat /home/level8/README.txt" >> /home/level8/.bashrc
 
 sudo echo "exit" >> /home/level8_ans/.bashrc;
 
