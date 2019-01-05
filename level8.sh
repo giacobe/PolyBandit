c9277ed0dbe6039f3e69c285aa45773d3f1e0303
Nick Giacobe <nxg13@ist.psu.edu>
2019-01-05 18:10:00 -0500

    Update level8.sh

--- level8.sh
+++ level8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-useradd level8_ans --create-home --password "$(openssl passwd -1 "password")" --shell /bin/bash --user-group
+# I don't think we need this - NG useradd level8_ans --create-home --password "$(openssl passwd -1 "password")" --shell /bin/bash --user-group
 
 echo "******************************************************************" >> /home/level8/README.txt;
 echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level8/README.txt;
@@ -11,16 +11,16 @@ echo "*                                                                *" >> /ho
 echo "* You are at Level 8                                             *" >> /home/level8/README.txt;
 echo "*                                                                *" >> /home/level8/README.txt;
 echo "* The password for the next level is in a file called inhere.txt *" >> /home/level8/README.txt;
-echo "* in level8_ans, but there is a problem when you try to log in.  *" >> /home/level8/README.txt;
+echo "* but there is a problem when you try to log in.                 *" >> /home/level8/README.txt;
 echo "*                                                                *" >> /home/level8/README.txt;
 echo "* When you get the password for the next level, log in to the    *" >> /home/level8/README.txt;
 echo "* next level with the command:                                   *" >> /home/level8/README.txt;
-echo "*         ssh level8@localhost                                   *" >> /home/level8/README.txt;
+echo "*         ssh level9@localhost                                   *" >> /home/level8/README.txt;
 echo "*                                                                *" >> /home/level8/README.txt;
 echo "******************************************************************" >> /home/level8/README.txt;
 echo "cat /home/level8/README.txt" >> /home/level8/.bashrc
 
-sudo echo "exit" >> /home/level8_ans/.bashrc;
+# this goes in Level 9's setup sudo echo "exit" >> /home/level9/.bashrc;
 
-echo $level9_pass > /home/level8_ans/inhere.txt;
-chown level8_ans:level8_ans /home/level8_ans/inhere.txt;
+echo $level9_pass > /home/level8/inhere.txt;
+chown level8_ans:level8 /home/level8/inhere.txt;
