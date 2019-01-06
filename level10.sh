e33a49d3992d14f67c21ad11f5ddc92ce3b31b42
Nick Giacobe <nxg13@ist.psu.edu>
2019-01-05 20:44:06 -0500

    Update level10.sh

--- level10.sh
+++ level10.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-echo "The password for the next level is the only line that's different between the two inhere files in level10_account1 and level10_account2. The password will be in the level10_account1's side." > /home/level10/README.txt;
 
-useradd level10_account1 --create-home --password "$(openssl passwd -1 "password")" --shell /bin/bash --user-group
+echo "******************************************************************" >> /home/level10/README.txt;
+echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level10/README.txt;
+echo "* Overthewire.org's Bandit exercise. The object is to figure out *" >> /home/level10/README.txt;
+echo "* what the password is for the next level, then log into that    *" >> /home/level10/README.txt;
+echo "* next level's account using SSH.                                *" >> /home/level10/README.txt;
+echo "*                                                                *" >> /home/level10/README.txt;
+echo "* You are at Level 10. This is the last level.                   *" >> /home/level10/README.txt;
+echo "*                                                                *" >> /home/level10/README.txt;
+echo "* The password to prove you won the game is the only line that's *" >> /home/level10/README.txt;
+echo "* different between the two files named inhere.                  *" >> /home/level10/README.txt;
+echo "*                                                                *" >> /home/level10/README.txt;
+echo "******************************************************************" >> /home/level10/README.txt;
 
-useradd level10_account2 --create-home --password "$(openssl passwd -1 "password")" --shell /bin/bash --user-group
+echo "cat /home/level10/README.txt" >> /home/level10/.bashrc
 
-touch /home/level10_account1/oneline.txt;
-touch /home/level10_account2/oneline.txt;
-touch /home/level10_account1/inhere.txt;
-touch /home/level10_account2/inhere.txt;
+#useradd level10_account1 --create-home --password "$(openssl passwd -1 "password")" --shell /bin/bash --user-group
+#useradd level10_account2 --create-home --password "$(openssl passwd -1 "password")" --shell /bin/bash --user-group
+
+touch /home/level10/inhere1.txt;
+touch /home/level10/inhere2.txt;
 
 declare -a LETTERS=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0");
 
@@ -20,10 +31,10 @@ do
 
   if (($count == $password_spot))
   then
-    echo $level10_pass > /home/level10_account1/oneline.txt;
-    cat /home/level10_account1/inhere.txt /home/level10_account1/oneline.txt > /home/level10_account1/inhere2.txt;
-    rm /home/level10_account1/inhere.txt;
-    mv /home/level10_account1/inhere2.txt /home/level10_account1/inhere.txt
+    echo $level10_pass >> /home/level10/inhere1.txt;
+    #cat /home/level10/inhere1.txt /home/level10/oneline.txt > /home/level10/inhere2.txt;
+    #rm /home/level10/inhere1.txt;
+    #mv /home/level10_account1/inhere2.txt /home/level10_account1/inhere.txt
   else
     char1=$[RANDOM%61];
     char2=$[RANDOM%61];
@@ -36,16 +47,11 @@ do
 
     rand_pass=${LETTERS[$char1]}${LETTERS[$char2]}${LETTERS[$char3]}${LETTERS[$char4]}${LETTERS[$char5]}${LETTERS[$char6]}${LETTERS[$char7]}${LETTERS[$char8]};
 
-    echo $rand_pass > /home/level10_account1/oneline.txt;
-    cat /home/level10_account1/inhere.txt /home/level10_account1/oneline.txt > /home/level10_account1/inhere2.txt;
-    rm /home/level10_account1/inhere.txt;
-    mv /home/level10_account1/inhere2.txt /home/level10_account1/inhere.txt
-    echo $rand_pass > /home/level10_account2/oneline.txt;
-    cat /home/level10_account2/inhere.txt /home/level10_account2/oneline.txt > /home/level10_account2/inhere2.txt;
-    rm /home/level10_account2/inhere.txt;
-    mv /home/level10_account2/inhere2.txt /home/level10_account2/inhere.txt
-    chown level10_account1:level10_account1 /home/level10_account1/inhere.txt;
-    chown level10_account2:level10_account2 /home/level10_account2/inhere.txt;
+    echo $rand_pass >> /home/level10/inhere1.txt;
+    echo $rand_pass >> /home/level10/inhere2.txt;
+
   fi
   count=$((count+1));
 done
+chown level10:level10 /home/level10/inhere1.txt;
+chown level10:level10 /home/level10/inhere2.txt;
