cbbae392213854db70ed1f3294e2561cbc3399f9
Nick Giacobe <nxg13@ist.psu.edu>
2022-06-15 00:14:26 -0400

    Update level7.sh

--- level7.sh
+++ level7.sh
@@ -4,7 +4,7 @@ echo "******************************************************************" >> /ho
 echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level7/README.txt;
 echo "* Overthewire.org's Bandit exercise. The object is to figure out *" >> /home/level7/README.txt;
 echo "* what the password is for the next level, then log into that    *" >> /home/level7/README.txt;
-echo "* next level's account using SSH.                                *" >> /home/level7/README.txt;
+echo "* next level's account using su.                                 *" >> /home/level7/README.txt;
 echo "*                                                                *" >> /home/level7/README.txt;
 echo "* You are at Level 7                                             *" >> /home/level7/README.txt;
 echo "*                                                                *" >> /home/level7/README.txt;
@@ -13,7 +13,7 @@ echo "* easy to read the file.                                         *" >> /ho
 echo "*                                                                *" >> /home/level7/README.txt;
 echo "* When you get the password for the next level, log in to the    *" >> /home/level7/README.txt;
 echo "* next level with the command:                                   *" >> /home/level7/README.txt;
-echo "*         ssh level8@localhost                                   *" >> /home/level7/README.txt;
+echo "*         su - level8                                            *" >> /home/level7/README.txt;
 echo "*                                                                *" >> /home/level7/README.txt;
 echo "******************************************************************" >> /home/level7/README.txt;
 echo "cat /home/level7/README.txt" >> /home/level7/.bashrc
