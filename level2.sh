442b82948de2fdcc517b0987b8b54c2e6a3a7328
Nick Giacobe <nxg13@ist.psu.edu>
2022-06-15 00:09:47 -0400

    Update level2.sh

--- level2.sh
+++ level2.sh
@@ -4,7 +4,7 @@ echo "******************************************************************" >> /ho
 echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level2/README.txt;
 echo "* Overthewire.org's Bandit exercise. The object is to figure out *" >> /home/level2/README.txt;
 echo "* what the password is for the next level, then log into that    *" >> /home/level2/README.txt;
-echo "* next level's account using SSH.                                *" >> /home/level2/README.txt;
+echo "* next level's account using su.                                 *" >> /home/level2/README.txt;
 echo "*                                                                *" >> /home/level2/README.txt;
 echo "* You are at Level 2                                             *" >> /home/level2/README.txt;
 echo "*                                                                *" >> /home/level2/README.txt;
@@ -12,7 +12,7 @@ echo "* The password for the next level is in a file with byte size 9  *" >> /ho
 echo "*                                                                *" >> /home/level2/README.txt;
 echo "* When you get the password for the next level, log in to the    *" >> /home/level2/README.txt;
 echo "* next level with the command:                                   *" >> /home/level2/README.txt;
-echo "*         ssh level3@localhost                                   *" >> /home/level2/README.txt;
+echo "*         su - level3                                            *" >> /home/level2/README.txt;
 echo "*                                                                *" >> /home/level2/README.txt;
 echo "******************************************************************" >> /home/level2/README.txt;
 
