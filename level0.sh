bf911e04359f8cb5e6def90dfa92ea83519c2dad
Nick Giacobe <nxg13@ist.psu.edu>
2022-06-15 00:08:16 -0400

    Update level0.sh

--- level0.sh
+++ level0.sh
@@ -4,7 +4,7 @@ echo "******************************************************************" >> /ho
 echo "* Welcome to PolyBandit. This is a polymorphic clone of          *" >> /home/level0/README.txt;
 echo "* Overthewire.org's Bandit exercise. The object is to figure out *" >> /home/level0/README.txt;
 echo "* what the password is for the next level, then log into that    *" >> /home/level0/README.txt;
-echo "* next level's account using SSH.                                *" >> /home/level0/README.txt;
+echo "* next level's account using su.                                 *" >> /home/level0/README.txt;
 echo "*                                                                *" >> /home/level0/README.txt;
 echo "* You are at Level 0                                             *" >> /home/level0/README.txt;
 echo "*                                                                *" >> /home/level0/README.txt;
@@ -12,7 +12,7 @@ echo "* The password for the next level is in a file called inhere.txt *" >> /ho
 echo "*                                                                *" >> /home/level0/README.txt;
 echo "* When you get the password for the next level, log in to the    *" >> /home/level0/README.txt;
 echo "* next level with the command:                                   *" >> /home/level0/README.txt;
-echo "*         ssh level1@localhost                                   *" >> /home/level0/README.txt;
+echo "*         su - level1                                            *" >> /home/level0/README.txt;
 echo "*                                                                *" >> /home/level0/README.txt;
 echo "******************************************************************" >> /home/level0/README.txt;
 echo "cat ~/README.txt" >> /home/level0/.bashrc
